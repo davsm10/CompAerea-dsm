d062c7dbfeb564a1124ace1bfff8ed16e7917125
daw1ed <DavidSM>
2023-11-30 01:57:26 +0100

    Primer commit

---
+++ Proyecto1_ED_DSM/src/es/iescanaveral/daw1/ed/dsm/Clase1ED.java
@@ -0,0 +1,13 @@
+package es.iescanaveral.daw1.ed.dsm;
+/*
+* @author DAW: David Sánchez Morón - IES El Cañaveral
+* Fecha: 3 nov 2023
+*/
+public class Clase1ED {
+
+	public static void main(String[] args) {
+        Clase2ED clase2 = new Clase2ED();
+        clase2.saludar("DSM");
+    }
+	
+}
---
+++ Proyecto1_ED_DSM/src/es/iescanaveral/daw1/ed/dsm/Clase2ED.java
@@ -0,0 +1,13 @@
+package es.iescanaveral.daw1.ed.dsm;
+/*
+* @author DAW: David Sánchez Morón - IES El Cañaveral
+* Fecha: 3 nov 2023
+*/
+public class Clase2ED {
+
+	public void saludar(String inicialesAlumno){
+        String textoSaludo= "Hola, soy " + inicialesAlumno + " imprimiendo un mensaje editado con Eclipse.";
+        System.out.println(textoSaludo);
+    }
+	
+}
